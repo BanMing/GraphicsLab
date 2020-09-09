e28035561da9a9b4d898dca084700624b6cb0fcb
BanMing <ban-ming@foxmail.com>
2020-09-09 17:53:33 +0800

    add

---
+++ d3dx12/README.md
@@ -0,0 +1,1 @@
+Introduction to 3D Game Programming with DirectX 12.pdf
--- d3dx12/d3dx12/GameTimer.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameTimer.h"
-GameTimer::GameTimer()
-	:mSecondsPerCount(0.f), mDeltaTime(-1.f), mBaseTime(0.f),
-	mPausedTime(0.f), mPrevTime(0.f), mCurrTime(0.f), mStopped(false)
-{
-	__int64 countsPerSec;
-	//QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
-	mSecondsPerCount = 1.0 / (double)countsPerSec;}
