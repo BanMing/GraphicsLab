7eeca807ede5ddf6bea707db3c20b1ad00a8cb1f
BanMing <ban-ming@foxmail.com>
2020-09-10 18:35:58 +0800

    add

--- d3dx12/chapter6/Exercises2/Exercises2/BoxApp.cpp
+++ d3dx12/chapter6/Exercises2/Exercises2/BoxApp.cpp
@@ -1,17 +1,17 @@
-﻿
-#include "../../Common/d3dApp.h"
-#include "../../Common/MathHelper.h"
-#include "../../Common/UploadBuffer.h"
+
+#include "../../../Common/d3dApp.h"
+#include "../../../Common/MathHelper.h"
+#include "../../../Common/UploadBuffer.h"
 
 using Microsoft::WRL::ComPtr;
 using namespace DirectX;
 using namespace DirectX::PackedVector;
 
-struct Vertex
-{
-	XMFLOAT3 Pos;
-	XMFLOAT4 Color;
-};
+//struct Vertex
+//{
+//	XMFLOAT3 Pos;
+//	XMFLOAT4 Color;
+//};
 
 struct ObjectConstants
 {
