18d9cd98e55ccd6f8d033a42b372eb32d2a79bb4
BanMing <ban-ming@foxmail.com>
2020-09-11 18:10:36 +0800

    ADD chapter

---
+++ d3dx12/chapter6/Exercises3/Vertex.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../../Common/d3dApp.h"
+using namespace DirectX;
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+struct ObjectConstants
+{
+	XMFLOAT4X4 WorldViewProj = MathHelper::Identity4x4();
+};
