f898427207bd9b877f78ec8d0465388492c82bbb
BanMing <ban-ming@foxmail.com>
2021-07-21 18:57:13 +0800

    add oneweekend ray tracing

---
+++ ray_tracing/oneweekend/vec3.h
@@ -0,0 +1,19 @@
+/******************************************************************
+** vec3.h
+** @Author       : BanMing 
+** @Date         : 7/21/2021, 6:55:42 PM
+** @Description  : 
+*******************************************************************/
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+};
+void Run() {}
+
+#endif
